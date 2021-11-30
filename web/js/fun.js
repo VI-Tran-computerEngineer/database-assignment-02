5414e1a6937e5232a6af9e3f804ffa16f784604e
Tran Long Vi <tranlongvi.2000@gmail.com>
2021-11-30 21:36:05 +0700

    Delete web directory

--- web/js/fun.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function( ) {
-    let button = document.querySelector('.btnAddNew');
-    let form = document.querySelector('.formAddNew');
-    button.onclick = function() {
-        form.classList.toggle("d-none");
-    }
-}, false );
